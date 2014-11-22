7b157fea5c415a13ae91584c86901a5a85a79ff9
mkoltsov <root@javabean.ru>
2014-11-22 17:46:32 +0300

    nothing to expect here

--- RedisInteraction.groovy
+++ RedisInteraction.groovy
@@ -1,7 +1,9 @@
 @Grab('redis.clients:jedis:2.6.0')
 import redis.clients.jedis.Jedis
-    
+
 def redisHost = '137.135.204.110'
 def jedis = new Jedis(redisHost)
 jedis.set("chef","pupa")
 println jedis.get("chef")
+jedis.set("pupa","chef")
+println jedis.get("pupa")
